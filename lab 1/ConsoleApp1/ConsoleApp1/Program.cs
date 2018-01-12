94df63e8ea25333284c02bfa38f9785e5c129ec3
sarsembin <daur2000@gmail.com>
2018-01-12 15:06:17 +0600

    first commit

---
+++ lab 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Rect
+    {
+        public int w, h;
+        //private int h;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rect r = new Rect();
+            int a = 2;//
+            r.w
+        }
+    }
+}
