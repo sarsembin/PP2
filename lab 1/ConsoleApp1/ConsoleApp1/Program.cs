7b858e717d84407ef88ef12fd0fa231c587ca329
sarsembin <daur2000@gmail.com>
2018-01-12 15:40:17 +0600

    1

--- lab 1/ConsoleApp1/ConsoleApp1/Program.cs
+++ lab 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,19 +6,38 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Rect
+    /*class Rect
     {
         public int w, h;
         //private int h;
 
-    }
+    }*/
     class Program
     {
         static void Main(string[] args)
         {
-            Rect r = new Rect();
-            int a = 2;//
-            r.w
+            //Rect r = new Rect();
+            //string s = Console.ReadLine();
+            //string[] sos = s.Split(' ');
+            int para;
+            for (int i=0;i < args.Length; i++)
+            {
+                bool kek = true;
+                para = int.Parse(args[i]);
+                for (int j = 2; j < para; j++)
+                {
+                    if (para % j == 0)
+                    {
+                        kek = false;
+                        break;
+                    }
+                }
+                if (kek == true)
+                {
+                    Console.WriteLine(para); 
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
