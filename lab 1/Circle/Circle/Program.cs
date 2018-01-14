ddaf1f7a4f7e0166f8b66d5b1f54f2552c6521a4
sarsembin <daur2000@gmail.com>
2018-01-14 22:20:56 +0600

    last

---
+++ lab 1/Circle/Circle/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Circle
+    {
+        public int radius;
+        public int D;
+        public double S;
+        public double L;
+        public Circle()
+        {
+            string r = Console.ReadLine();
+            radius = int.Parse(r);
+        }
+        public int diam()
+        {
+            D = radius * 2;
+            return D;
+        }
+        public double area()
+        {
+            S = 3.14 * radius * radius;
+            return S;
+        }
+        public double length()
+        {
+            L = 2 * 3.14 * radius;
+            return L;
+        }
+        public override string ToString()
+        {
+            return radius + "\n" + D + "\n" + L + "\n" + S;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle C = new Circle();
+            C.diam();
+            C.length();
+            C.area();
+            Console.WriteLine(C);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab 1/Rectangle/Rectangle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Rect
+    {
+        public int width;
+        public int height;
+        public int S;
+        public int P;
+        public Rect()
+        {
+            string w = Console.ReadLine();
+            width = int.Parse(w);
+            string h = Console.ReadLine();
+            height = int.Parse(h);
+        }
+        public int Area()
+        {
+            S = width * height;
+            return S;
+        }
+        public int Perim()
+        {
+            P = 2 * (width + height);
+            return P;
+        }
+        public override string ToString()
+        {
+            return width+"\n"+height+"\n" + S + "\n" + P;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rect r = new Rect();
+            r.Perim();
+            r.Area();
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
