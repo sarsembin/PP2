53ef675730dc67af900a5d8b98aeaad4aad302ad
sarsembin <daur2000@gmail.com>
2018-01-29 11:14:19 +0600

    week3

---
+++ lab 2/StackRe/StackRe/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackRe
+{
+    class Program
+    {
+        static void show_files()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Daur\Desktop\acmp");
+            FileInfo[] files = dir.GetFiles();
+            for (int i=0;i<files.Length; i++)
+            {
+                Console.WriteLine(files[i].Name);
+            }
+        }
+        static void show_dirs()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Daur\Desktop\acmp");
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                Console.WriteLine(dirs[i].Name);
+            }
+        }
+        static void show_ever(string path, int depth = 0)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] files = dir.GetFiles();
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            Stack s1 = new Stack();
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                s1.Push(dirs[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            show_ever(@"C:\Users\Daur\Desktop\acmp");
+        }
+    }
+}
---
+++ lab 3/far/far/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace far
+{
+    class Program
+    {
+        public static int kek;
+        static void show_all(DirectoryInfo dir,int pos)
+        {
+            FileSystemInfo[] names = dir.GetFileSystemInfos();
+            kek = names.Length;
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (i == pos)
+                { 
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                if (names[i].GetType() == typeof(DirectoryInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                Console.WriteLine(names[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int pos = 0;
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Daur\Desktop\acmp");
+            while (true)
+            {
+                Console.Clear();
+                show_all(dir, pos);
+                ConsoleKeyInfo button = Console.ReadKey();
+                switch (button.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        pos--;
+                        if (pos < 0)
+                        {
+                            pos = kek - 1;
+                            Console.BackgroundColor = ConsoleColor.Black;
+                        }
+                        else if (pos == kek - 2)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            //Console.BackgroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        pos++;
+                        if (pos > kek-1)
+                        {
+                            pos = 0;
+                            Console.BackgroundColor = ConsoleColor.Black;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        FileSystemInfo f = dir.GetFileSystemInfos()[pos];
+                        if (f.GetType() == typeof(DirectoryInfo))
+                        {
+                            dir = new DirectoryInfo(f.FullName);
+                            pos = 0;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            FileStream fs = new FileStream(f.FullName, FileMode.Open, FileAccess.Read);
+                            StreamReader sr = new StreamReader(fs);
+                            string data = sr.ReadToEnd();
+                            Console.Clear();
+                            Console.WriteLine(data);
+                            Console.ReadKey();
+                            
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        dir = dir.Parent;
+                        pos = 0;
+                        break;
+                }
+            }
+
+        }
+    }
+}
